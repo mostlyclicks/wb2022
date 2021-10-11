614e317e45ab9227c131d4912aff09ff6bf1c234
mostlyclicks <carlos@mostlyclicks.com>
2021-10-11 12:27:26 -0600

    get prismic connect - config plugin and tokens

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.wieserbrothers.com",
     title: "wb2022",
   },
   plugins: [
     "gatsby-plugin-image",
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        //linkResolver: (doc) => linkResolver(doc),
+      },
+    },
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
