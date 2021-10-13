ace9df030330e3ce19e55ed723e040cf8b654cd8
mostlyclicks <carlos@mostlyclicks.com>
2021-10-13 09:04:04 -0600

    get project index working

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-const index = () => {
+const index = ({data}) => {
+
+  const projectData = data.allPrismicProject.edges
+
   return (
     <div>
       <h1>Project Index</h1>
       <p>Might do this by category so this is all temp.</p>
+
+      {projectData.map((project) => {
+
+        return (
+          <p>
+            <Link to={project.node.uid}>{project.node.data.title.text}</Link>
+          </p>
+          
+        )
+      })}
+
     </div>
   )
 }
@@ -14,4 +29,21 @@ export default index
 //create index for each category
 //Or use a filter in graphql
 
+
+export const ProjectQuery = graphql`
+  query {
+    allPrismicProject {
+      edges {
+        node {
+          uid
+          data {
+            title {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
