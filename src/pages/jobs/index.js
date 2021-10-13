f90bd283d22f58003f6d11766591397f927e9b41
mostlyclicks <carlos@mostlyclicks.com>
2021-10-12 21:12:50 -0600

    add emp opportunities index query

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const index = () => {
+
+const index = ({data}) => {
+  
+  const JobsData = data.allPrismicEmploymentOpportunity.edges
+  
   return (
     <div>
       <h1>Employment Opportunities Index</h1>
+      {JobsData.map((job) => {
+
+        return (
+          <h2>{job.node.data.title.text}</h2>
+        )
+      })}
     </div>
   )
 }
@@ -12,3 +23,19 @@ export default index
 
 //Query all jobs 
 //need title and slug
+
+export const JobData = graphql`
+  query {
+    allPrismicEmploymentOpportunity {
+      edges {
+        node {
+          data {
+            title {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/news-events/index.js
+++ src/pages/news-events/index.js
@@ -1,47 +1,36 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
 
-
 const Index = ({data}) => {
 
   const NewsData = data.allPrismicNewsAndEvents.edges
-  console.log(NewsData)
-
+  
   return (
     <div>
       <h1>News and Events Index</h1>
-      {/*  */}
       {NewsData.map((article) => {
-
         return (
           <h2><Link to={article.node.uid}>{article.node.data.title.text}</Link></h2>
         )
       })}
-     
     </div>
   )
 }
 export default Index
 
-
-//get all news and events
-//may need to paginate ... arghhhhh
-//Get slug/Id and title
-
-
 export const NewsEventsQuery = graphql`
-query {
-  allPrismicNewsAndEvents {
-    edges {
-      node {
-        uid
-        data {
-          title {
-            text
+  query {
+    allPrismicNewsAndEvents {
+      edges {
+        node {
+          uid
+          data {
+            title {
+              text
+            }
           }
         }
       }
     }
   }
-}
 `
