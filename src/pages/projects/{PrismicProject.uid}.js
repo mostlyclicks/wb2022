1e4856b509b7a2ea1de8811fb0dbfe0f53d752e1
mostlyclicks <carlos@mostlyclicks.com>
2021-10-13 11:23:25 -0600

    get project query working

--- src/pages/projects/{PrismicProject.uid}.js
+++ src/pages/projects/{PrismicProject.uid}.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const Project = () => {
+const Project = ({data}) => {
+
+  const project = data.prismicProject
+
   return (
     <div>
       <h1>Project Template</h1>
+
+      {project.data.title.text}
+
     </div>
   )
 }
 
 export default Project
+
+
+export const query = graphql`
+  query ProjectQuery($uid: String) {
+    prismicProject(uid: {eq: $uid}) {
+      data {
+        title {
+          text
+        }
+      }
+    }
+  }
+`
