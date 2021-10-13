d7706c6a969df0da6fba317886dcb0573c8fe610
mostlyclicks <carlos@mostlyclicks.com>
2021-10-12 20:53:23 -0600

    get news events index working

--- src/pages/news-events/index.js
+++ src/pages/news-events/index.js
@@ -1,16 +1,45 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const index = () => {
+const Index = ({data}) => {
+
+  const NewsData = data.allPrismicNewsAndEvents.edges
+  console.log(NewsData)
+
   return (
     <div>
       <h1>News and Events Index</h1>
+      {/*  */}
+      {NewsData.map((article) => {
+
+        return (
+          <h2>{article.node.data.title.text}</h2>
+        )
+      })}
+     
     </div>
   )
 }
-
-export default index
+export default Index
 
 
 //get all news and events
 //may need to paginate ... arghhhhh
 //Get slug/Id and title
+
+
+export const NewsEventsQuery = graphql`
+query {
+  allPrismicNewsAndEvents {
+    edges {
+      node {
+        data {
+          title {
+            text
+          }
+        }
+      }
+    }
+  }
+}
+`
--- src/pages/news-events/{news-event.slug}.js
+++ src/pages/news-events/{news-event.slug}.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const NewsEventsPost = () => {
+
+
+
   return (
     <div>
       <h1>News/Events Template</h1>
+      
     </div>
   )
 }
