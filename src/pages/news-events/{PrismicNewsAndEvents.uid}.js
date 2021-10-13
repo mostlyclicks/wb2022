686b70e129a2e9e5a2457f0e4c92887f75a7b149
mostlyclicks <carlos@mostlyclicks.com>
2021-10-13 11:30:21 -0600

    get news article working

--- src/pages/news-events/{PrismicNewsAndEvents.uid}.js
+++ src/pages/news-events/{PrismicNewsAndEvents.uid}.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const NewsEventsPost = () => {
+const NewsEventsPost = ({data}) => {
 
-
+  const article = data.prismicNewsAndEvents
 
   return (
     <div>
       <h1>News/Events Template</h1>
-      
+      <div>
+        {article.data.title.text}
+      </div>
     </div>
   )
 }
@@ -17,4 +20,16 @@ export default NewsEventsPost
 
 //News Events Post Template
 //use props slug to get rest of context
+
+export const query = graphql`
+  query NewsEventQuery($uid: String) {
+    prismicNewsAndEvents(uid: {eq: $uid}) {
+      data {
+        title {
+          text
+        }
+      }
+    }
+  }
+`
 
