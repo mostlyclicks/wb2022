e8fe40625239bdc2f1342c9190813381decf9283
mostlyclicks <carlos@mostlyclicks.com>
2021-11-03 16:06:21 -0600

    add real links to top nav

--- src/components/Layout/top-nav.js
+++ src/components/Layout/top-nav.js
@@ -1,17 +1,17 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const TopNav = () => {
   return (
     <div className="px-3 py-1.5 mb-5" style={{backgroundColor: '#3d3d3d'}}>
       <ul className="flex justify-center md:justify-end flex-wrap gap-6 ">
-        <li className="hidden md:block">COVID-19 UPDATE</li>
-        <li className="hidden md:block">Careers</li>
-        <li>Submit Bids</li>
-        <li>Info</li>
-        <li className="hidden md:block">Contact Us</li>
-        <li>507.895.8903</li>
+        <li className="hidden md:block"><Link to="/">COVID-19 UPDATE</Link></li>
+        <li className="hidden md:block"><Link to ="/jobs">Careers</Link></li>
+        <li><a href="mailto:bids@wieserbrothers.com">Submit Bids</a></li>
+        <li><a href="mailto:info@wieserbrothers.com">Info</a></li>
+        <li className="hidden md:block"><Link to="/contact-us">Contact Us</Link></li>
+        <li><a href="tel:+1-507-895-8903">507.895.8903</a></li>
       </ul>
-      
     </div>
   )
 }
