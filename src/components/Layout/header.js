6839e7558be16483a1644b6685743fd54f98fd6a
mostlyclicks <carlos@mostlyclicks.com>
2021-11-02 19:30:48 -0600

    header work start

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -7,11 +7,15 @@ const Header = () => {
   return (
     <div style={{backgroundColor: 'darkgreen', color: 'white'}}>
     <TopNav />
-      <nav className="flex justify-between">
+      <nav className="flex justify-between p-8">
         <StaticImage src="../../images/wieser-brothers-logo-w.png" alt="Wieser Brothers Logo" />
-        <ul className="flex flex-row justify-between">
+        <ul className="flex flex-row gap-4">
           <li><Link to="/">Home</Link></li>
           <li>Firm</li>
+          <li>Services</li>
+          <li>Commitment</li>
+          <li>Projects</li>
+          <li>News & Events</li>
         </ul>
       </nav>
     </div>
