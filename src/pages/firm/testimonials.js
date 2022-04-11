58c6a7e34f3f13067a0d1fca2036127dd9a707fb
mostlyclicks <carlos@mostlyclicks.com>
2022-04-10 21:11:52 -0600

    fix testimonial

--- src/pages/firm/testimonials.js
+++ src/pages/firm/testimonials.js
@@ -8,25 +8,21 @@ import Layout from "../../components/Layout/Layout"
 
 const Testimonials = ( {data} ) => {
 
-  const testimonialList = data.prismic.allTestimonials.edges
-  const testimonialCount = data.prismic.allTestimonials.totalCount
+  const testimonialList = data.allPrismicTestimonial.edges
+  // const testimonialCount = data.allPrismicTestimonial.totalCount
 
-  const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
 
-  const singleTestimonial = data.prismic.allTestimonials.edges[getRandomInt(testimonialCount)]
   
   return (
     <Layout>
       <h1>Testimonials</h1>
-      <p>{RichText.asText(singleTestimonial.node.title)}</p>
+      
       
       <ul>
         {testimonialList.map(quote => {
           return (
             <li>
-              <Link to={`/firm/testimonials/${quote.node._meta.uid}`}>
+              <Link to={`/firm/testimonials/${quote.node.uid}`}>
                 {RichText.asText(quote.node.title)} -{" "}
                 {RichText.asText(quote.node.person_quoted)}
               </Link>
@@ -46,6 +42,7 @@ export const query = graphql`
     allPrismicTestimonial {
       edges {
         node {
+          uid
           data {
             person_quoted {
               text
